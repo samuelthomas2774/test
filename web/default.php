faeeafa2cc80b73600b64f8c2475a07e0321e280
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2015-08-10 21:12:23 +0100

    Update default.php

--- web/default.php
+++ web/default.php
@@ -1,6 +1,13 @@
 <?php
-	if(isset($_SERVER["PATH_INFO"]) && ($_SERVER["PATH_INFO"] == "/phpinfo")) {
+	if(!isset($_SERVER["PATH_INFO"])) $_SERVER["PATH_INFO"] = "";
+	if($_SERVER["PATH_INFO"] == "/phpinfo") {
 		phpinfo();
+	} elseif($_SERVER["PATH_INFO"] == "/server") {
+		var_dump($_SERVER);
+	} elseif($_SERVER["PATH_INFO"] == "/environment") {
+		var_dump($_ENV);
+	} elseif($_SERVER["PATH_INFO"] == "/variables") {
+		var_dump($GLOBALS);
 	} else {
 		echo "<!DOCTYPE html>";
 		echo 5 * 2 + 1;
